2122e97af00fb1bd5373527389a756c9ecce72c8
hazymist <54222236+hazymist@users.noreply.github.com>
2022-08-21 10:33:11 -0400

    Update aut.py

--- aut.py
+++ aut.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 browser = webdriver.Chrome('C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe')
 browser.get('https://www.bing.com')   #Using selenium to go from bing to Youtube
 
-typing = browser.find_element_by_id('sb_form_q')
+typing = browser.find_element_by_id('sb_form_q')  #This is referencing an HTML tag within the site.
 
 
 typing.send_keys('kim possible theme song') #Simulating typing here, this line here is writing text on youtube search bar
